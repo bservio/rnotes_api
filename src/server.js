84912102e611bbe197c406eaff596dcf651c91f6
Bruno Sérvio Barbosa <brunoservio21@gmail.com>
2023-05-12 13:59:32 -0300

    cors update 3

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ migrationsRun();
 const app = express();
 
 var corsOptions = {
-	origin: 'https://rocketnotes-api-5vfb.onrender.com$/',
+	origin: true,
 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions));
